fdac9e3b44dd5420b483e9434d51fde7be323783
viyan-najmaldin <najmadinviyan@gmail.com>
2022-10-02 21:54:01 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleForm(e.target.new_task_description.value);
+    form.reset();
+  });
 });
+
+function handleForm(todo) {
+  let list = document.getElementById("tasks");
+  let li = document.createElement("li");
+  let deleteButton = document.createElement("button");
+  let span = document.createElement("span");
+  span.innerHTML = "     ";
+  li.innerHTML = `${todo}`;
+  deleteButton.innerHTML = "X";
+  li.appendChild(span);
+  li.appendChild(deleteButton);
+  deleteButton.addEventListener("click", handleDelete);
+  list.appendChild(li);
+}
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
